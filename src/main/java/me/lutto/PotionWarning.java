fbbae8c2b3a86600e13c6af4f4e77e0604ce42d6
StillLutto <iameinsteinhe@gmail.com>
2024-05-19 22:39:10 +0200

    Initial Commit

---
+++ src/main/java/me/lutto/PotionWarning.java
@@ -0,0 +1,12 @@
+package me.lutto;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PotionWarning implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        
+    }
+
+}
---
+++ src/main/java/me/lutto/client/PotionWarningClient.java
@@ -0,0 +1,12 @@
+package me.lutto.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PotionWarningClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
