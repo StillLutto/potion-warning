b5cb87195793584781fcf2bf48d115717622813c
StillLutto <iameinsteinhe@gmail.com>
2024-05-19 22:47:07 +0200

    feat: add mixin to check for status effect duration updates

---
+++ src/main/java/me/lutto/mixin/StatusEffectInstanceMixin.java
@@ -0,0 +1,22 @@
+package me.lutto.mixin;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(StatusEffectInstance.class)
+public class StatusEffectInstanceMixin {
+
+    @Shadow
+    private int duration;
+
+    @Inject(at = @At("TAIL"), method = "updateDuration")
+    private void injectDuration(CallbackInfoReturnable<Integer> cir) {
+        if (duration != 0) return;
+        // Further logic here
+    }
+
+}
