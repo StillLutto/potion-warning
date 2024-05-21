3e832b50589bc556711f864e00eaa2c13855f54b
StillLutto <iameinsteinhe@gmail.com>
2024-05-21 14:56:04 +0200

    feat(effect-mixin): check if entity is player

--- src/main/java/me/lutto/mixin/StatusEffectInstanceMixin.java
+++ src/main/java/me/lutto/mixin/StatusEffectInstanceMixin.java
@@ -1,9 +1,17 @@
 package me.lutto.mixin;
 
 import me.lutto.client.PotionWarningClient;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,6 +31,9 @@ public class StatusEffectInstanceMixin {
     private void injectUpdate(LivingEntity entity, Runnable overwriteCallback, CallbackInfoReturnable<Boolean> cir) {
         if (duration != 0) return;
 
+        PlayerEntity thisEntity = (PlayerEntity) entity;
+        PlayerEntity localPlayer = MinecraftClient.getInstance().player;
+        if (!thisEntity.equals(localPlayer)) return;
         PotionWarningClient.getStatusEffectHudManager().triggerHudDisplay(type);
     }
 
